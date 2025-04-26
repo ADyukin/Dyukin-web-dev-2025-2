2603bc4597f6e9d7319967c369932cf48c68794f
ADyukin <andreydyukin@mail.ru>
2025-04-26 15:14:13 +0300

    add

--- lab4/app/config.py
+++ lab4/app/config.py
@@ -1,6 +1,6 @@
 SECRET_KEY = 'ecc534bda547719a61b928f48429f880e6d6cb7f03bd489cfebba7926f9571cb'
 
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = ''
-MYSQL_HOST = 'localhost'
-MYSQL_DATABASE = 'std_900_labs_2025'
+MYSQL_USER = 'adyukin'
+MYSQL_PASSWORD = 'webmysql'
+MYSQL_HOST = 'adyukin.mysql.pythonanywhere-services.com'
+MYSQL_DATABASE = 'adyukin$default'
